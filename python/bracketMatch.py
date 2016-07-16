45b6a9e2710a5a6135d8f3085ad174712d7cff07
biwen <wangbiwen_88@qq.com>
2016-07-16 22:43:09 +0800

    add a code file.

---
+++ python/bracketMatch.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#########################################################################
+# File Name: bracketMatch.py
+# Author: Wang Biwen
+# mail: wangbiwen88@126.com
+# Created Time: 2016.06.27
+#########################################################################
+
+class BracketMatch(object):
+    def __init__(self, s):
+        bracketStr = s
+    def isMatch(self):
+        for s in self.bracketStr:
+            if s in set
+
